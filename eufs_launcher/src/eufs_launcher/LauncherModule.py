28d8f041e4fcbd1778bcfc82f7ff491b5220f555
AlliterativeAnchovies <alliterative.anchovies@gmail.com>
2019-02-06 17:39:42 +0000

    Generator mostly abides by contest regulations
    Still Extant Problems: Intersects itself, doesn't loop back

--- eufs_launcher/src/eufs_launcher/LauncherModule.py
+++ eufs_launcher/src/eufs_launcher/LauncherModule.py
@@ -29,35 +29,7 @@ class EufsLauncher(Plugin):
 		#(Not my fault, really - it's because Kinetic lacks the argument-passing feature for
 		#roslaunch, meaning I have to Popen it instead, and Popen.kill doesn't successfully kill
 		#all ros nodes)
-		"""
-		extranodes = rosnode.get_node_names()
-		extranodes.remove('/rosout')
-		try:
-			extranodes.remove('/gazebo')
-		except ValueError:
-			pass
-		try:
-			extranodes.remove('/gazebo_gui')
-		except ValueError:
-			pass
-		extranodes = [f for f in extranodes if f[:17] != '/rqt_gui_py_node_'] #remove the gui's node from list
-		if len(extranodes) > 0:
-			print("\n\n\nWarning: extraneous nodes detected:")
-			print(extranodes)
-			print("Continuing may produce errors!")
-			print("[This was likely because you shutdown the launcher incorrectly - best way is to ctrl-c in the terminal.]")
-			answer = str(raw_input("Do you want to kill all nodes? Y or y for yes, else no: "))
-			if answer.capitalize() == 'Y':
-				print("Killing nodes...")
 
-				#You would think rosnode's api would kill nodes correctly
-				#but you would be wrong
-				#rosnode.kill_nodes(extranodes)
-
-				#for badnode in extranodes:
-				#	Popen(["rosnode","kill",badnode])
-				Popen(["killall","-9","gzserver"])
-		"""
 
 		#Worst case scenario kill method: killall -9 gzserver gzclient
 		#We just have to nuke it.  I've tried so much, but gazebo is incredibly unstable
@@ -121,7 +93,7 @@ class EufsLauncher(Plugin):
 		if "rand.png" in imagefiles:
 			self._widget.findChild(QComboBox,"WhichImage").addItem("rand.png")
 		for f in imagefiles:
-			if f != "rand.png":
+			if f != "rand.png" and f[-3:] == "png":
 				self._widget.findChild(QComboBox,"WhichImage").addItem(f)
 
 		# Hook up buttons to onclick functions
@@ -146,56 +118,33 @@ class EufsLauncher(Plugin):
 		self.trackinner = (255,0,0,255)		#red
 		self.trackouter = (255,255,0,255)	#yellow
 
-		#Hide close button:
-		#self._widget.setWindowFlag(Qt.)
-
-
-		print("Plugin Successfully Launched!")
+		#Miscelaneous final initializations:
 		self.hasLaunchedROS = False
 		self.launchfileoverride = None
 		self.popenprocess = None
+
+		#Add experimental warning to generator
+		self._widget.findChild(QPushButton,"GenerateButton").setText("Generate Random Track\n(Experimental)")
+
+		print("Plugin Successfully Launched!")
 
 
 	def generator_button_pressed(self):
 		GENERATED_FILENAME = "rand"
 		print("Generating Track...")
-		twidth = 100
-		theight = 100
+
+		#Generate the track as pure data
+		(xys,twidth,theight) = generateAutocrossTrackdriveTrack((0,0))
+
+		#Create image to hold data
 		im = Image.new('RGBA', (twidth, theight), (0, 0, 0, 0)) 
 		draw = ImageDraw.Draw(im)
-		ts = [t/100.0 for t in range(101)]
 
-		#(All comments assume twidth = theight = 100 because I wrote them before I parameterized it)
-		#Let us have a fixed start at (100,50) and end at (0,50) [we'll draw curve flipped for ease of maths]
-		#and generate random control points
-		#First choose a number of control points "nc" from 5 to 15
-		#At (100/nc)*c for all control points c, choose random y from 0 to 100
-		nc = randrange(5,15)
-		xys = []
-		closedloop = randrange(0,10)>=5
-
-		if not closedloop:
-			#Just generate a random bezier curve
-			for c in range(0,nc+1):
-				xys = xys + [(5+c*(twidth-10)/nc,uniform(5,theight-10))]
-		else:
-			#Generate a bezier that doubles back on itself
-			for c in range(0,nc+1):
-				xys = xys + [(5+c*(twidth-10)/nc,uniform(5,theight/2))]
-			for c in range(0,nc):
-				xys = xys + [(5+(nc-c)*(twidth-10)/nc,uniform(theight/2,theight-10))] + [xys[0]]
-
-		bezier = make_bezier(xys)
-		points = bezier(ts)
-
-		#xys = [(end, point), (other randomly, generated points), (start, point)] to close the track into a loop!
-		#bezier = make_bezier(xys) #Probably better if done like a triangle with 3 points instead.
-		#points.extend(bezier(ts))
-
+		#Convert data to image format
 		draw.polygon([(0,0),(twidth,0),(twidth,theight),(0,theight),(0,0)],fill='white')#background
-		draw.line(points,fill=self.trackouter,width=5)#track full width
-		draw.line(points,fill=self.trackinner,width=3)#track innards
-		draw.line(points,fill=self.trackcenter)#track center
+		draw.line(xys,fill=self.trackouter,width=5)#track full width
+		draw.line(xys,fill=self.trackinner,width=3)#track innards
+		draw.line(xys,fill=self.trackcenter)#track center
 
 		#We want to calculate direction of car position -
 		sx = xys[0][0]
@@ -214,29 +163,40 @@ class EufsLauncher(Plugin):
 
 		#Now we want to make all pixels boardering the track become magenta (255,0,255) - this will be our 'cone' color
 		#To find pixel boardering track, simply find white pixel adjacent to a non-white non-magenta pixle
-		#If this is too thick, just destroy every second cone (or every third, or every fourth...  something like that!)
-		#(For efficiency's sake we're actually looking for non-white non-magenta pixels, and making every white adjacent become magenta.	
+		#We will also make sure not to put cones next to eachother	
 
+		conecolor_locations = []
 
 		pixels = im.load()#get reference to pixel data
+
+		def istrack(c):
+			return c == self.trackouter or c == self.trackinner or c == self.trackcenter
+
 		for i in range(im.size[0]):
 			for j in range(im.size[1]):
-				p = pixels[i,j]
-				if p != self.bgcolor and p != self.conecolor:
-					if i!=0 and j!=0:
-						if pixels[i-1,j-1] == self.bgcolor:
-							pixels[i-1,j-1] = self.conecolor
-					if i!=twidth-1 and j!=0:
-						if pixels[i+1,j-1] == self.bgcolor:
-							pixels[i+1,j-1] = self.conecolor
-					if i!=twidth-1 and j!=theight-1:
-						if pixels[i+1,j+1] == self.bgcolor:
-							pixels[i+1,j+1] = self.conecolor
-					if i!=0 and j!=theight-1:
-						if pixels[i-1,j+1] == self.bgcolor:
-							pixels[i-1,j+1] = self.conecolor
+				if pixels[i,j] == self.bgcolor:
+					isNextToTrack = False
+					isNextToCone = False
+					if i>0:
+						p=pixels[i-1,j]
+						isNextToTrack = isNextToTrack or istrack(p)
+						isNextToCone  = isNextToCone  or p==self.conecolor
+					if j>0:
+						p=pixels[i,j-1]
+						isNextToTrack = isNextToTrack or istrack(p)
+						isNextToCone  = isNextToCone  or p==self.conecolor
+					if i<twidth-1:
+						p=pixels[i+1,j]
+						isNextToTrack = isNextToTrack or istrack(p)
+						isNextToCone  = isNextToCone  or p==self.conecolor
+					if j<theight-1:
+						p=pixels[i,j+1]
+						isNextToTrack = isNextToTrack or istrack(p)
+						isNextToCone  = isNextToCone  or p==self.conecolor
+					if isNextToTrack and not isNextToCone:
+						pixels[i,j] = self.conecolor
 
-		#Finally, we just need to place noise.  At maximal noise, the track should be maybe 10% covered? (that's actually quite a lot!)
+		#Finally, we just need to place noise.  At maximal noise, the track should be maybe 1% covered? (that's actually quite a lot!)
 
 		for i in range(im.size[0]):
 			for j in range(im.size[1]):
@@ -247,12 +207,13 @@ class EufsLauncher(Plugin):
 		im.save(os.path.join(rospkg.RosPack().get_path('eufs_gazebo'), 'randgen_imgs/'+GENERATED_FILENAME+'.png'))
 		print("Image Generation Done, Converting to .world...")
 
-		self.create_track_from_image(im,GENERATED_FILENAME)
+		#self.create_track_from_image(im,GENERATED_FILENAME)
 
 		print("Track Gen Complete!")
 
-		self.launchfileoverride = GENERATED_FILENAME + ".launch"
-		self.launch_button_pressed()
+		im.show()
+		#self.launchfileoverride = GENERATED_FILENAME + ".launch"
+		#self.launch_button_pressed()
 
 	def track_from_image_button_pressed(self):
 		fname = self._widget.findChild(QComboBox,"WhichImage").currentText()
@@ -355,7 +316,6 @@ class EufsLauncher(Plugin):
 		noisefiles = open(os.path.join(rospkg.RosPack().get_path('eufs_launcher'), 'resource/noiseFiles.txt'),"r")
 		noisefiles = ("".join(noisefiles)).split("$===$")[1].strip().split("\n")
 		noiseweightings_ = [line.split("|") for line in noisefiles]
-		print(noisefiles)
 		noiseweightings  = [(float(line[0]),line[1]) for line in noiseweightings_]
 
 		def getRandomNoiseModel():
@@ -486,6 +446,177 @@ class EufsLauncher(Plugin):
 		# Comment in to signal that the plugin has a way to configure
 		# This will enable a setting button (gear icon) in each dock widget title bar
 		# Usually used to open a modal configuration dialog
+
+def endtangent(xys):
+	#Calculate direction of outgoing tangent of a set of points
+	sx = xys[-2][0]
+	sy = xys[-2][1]
+	ex = xys[-1][0]
+	ey = xys[-1][1]
+	return math.atan2((ey-sy),(ex-sx))
+
+def generateAutocrossTrackdriveTrack(startpoint):
+	#The rules:
+	#	Straights: 		<=80 meters
+	#	Constant Turns:		<=25 meter radius
+	#	Hairpin Turns: 		<=4.5 meter outside radius
+	#	Slalom: 		Cones in a straight line with 7.5 to 12 meter spacing [NOTE: can't be generated at this point, added later]
+	#	Track Width: 		>=3 meters
+	#	Track Length: 		<=1500 meters
+	xys = []
+	curTrackLength = 0
+
+	#Let's start with a straght
+	(generated, curpoint,deltalength) = generateStraight(startpoint,uniform(10,80),uniform(-math.pi,math.pi))
+	curTrackLength += deltalength
+	xys.extend(generated)
+
+	#Add on a cturn
+	(generated, curpoint,deltalength) = generateConstantTurn(curpoint,uniform(10,25),endtangent(xys))
+	curTrackLength += deltalength
+	xys.extend(generated)
+
+	#Add hairpins
+	(generated, curpoint,deltalength) = generateHairpinTurn(curpoint,uniform(4.5,10),endtangent(xys))
+	curTrackLength += deltalength
+	xys.extend(generated)
+
+	#TODO: Have straights, cturns, and hairpins placed in a random order
+
+	return convertPointsToAllPositive(xys)
+
+def generateHairpinTurn(startpoint,radius,intangent,switchbacknum=None,turnleft=None,wobbliness=None,straightsize=None):
+	curpoint = startpoint
+	curtangent = intangent
+	length = 0
+	startleftnum = 0 if turnleft else 1
+	
+	#A hairpin has a few choices:
+	#	How many switchbacks
+	#	Direction of first switchback
+	#	"Wobbliness" (circlepercent)
+	#	Size of straightways
+	turnleft = uniform(0,1)<0.5 		if turnleft == None 		else turnleft
+	switchbacknum = randrange(2,5)		if switchbacknum == None	else switchbacknum
+	wobbliness = uniform(0.4,0.6)		if wobbliness == None		else wobbliness
+	straightsize = uniform(10,80)		if straightsize == None		else straightsize
+
+	points = []
+	for a in range(0,switchbacknum):
+		#Switchback starts with a circle, then a line
+		#then we repeat
+
+		#cturn
+		(generated, curpoint,deltalength) = generateConstantTurn(curpoint,uniform(10,25),curtangent,circlepercent=wobbliness,turnleft=(a%2==startleftnum))
+		length += deltalength
+		points.extend(generated)
+		curtangent = endtangent(points)
+
+		#straight
+		(generated, curpoint,deltalength) = generateStraight(curpoint,straightsize,curtangent)
+		length += deltalength
+		points.extend(generated)
+		curtangent = endtangent(points)
+
+
+	#Returns a list of points and the new edge of the racetrack and the change in length
+	return (points,points[-1],length)
+
+def generateConstantTurn(startpoint,radius,intangent,turnleft=None,circlepercent=None):
+	(startx,starty) = startpoint
+
+	#cturns have a choices - turn left or turn right
+	#Then, they can choose what percent of the circle do they want to turn?
+	turnleft = uniform(0,1)<0.5 		if turnleft == None 		else turnleft
+	circlepercent = uniform(0.1,0.5)	if circlepercent == None 	else circlepercent
+
+	#Calculating this is fairly complicated
+	#Angle of circle normal = 90 degrees + intangent
+	#Slope of normal = -tan(90+normal) if turn left or tan(90+normal) if turn right
+	#Center is at startpoint + (r/sqrt( 1+m^2 )   ,   m*r*sqrt( 1+m^2 )) 
+	circnorm = math.pi/2 + intangent
+	slope    = math.tan(circnorm)
+	purex = radius/math.sqrt( 1+slope*slope )
+	if turnleft:
+		purex*=-1
+
+	centerx = startx + purex
+	centery = starty + slope*purex
+
+	#Now we use a rotation matrix to parameterize intermediate points:
+	#Given start S and center C, any point on the circle angle A away is:
+	#R_A[S-C] + C
+	#Let us box this up in a nice function:
+	def intermediatePoint(s,c,a):
+		(sx,sy) = s
+		(cx,cy) = c
+		cosa    = math.cos(a)
+		sina    = math.sin(a)
+		delx    = sx-cx
+		dely    = sy-cy
+		resultx = cosa*delx-sina*dely+cx
+		resulty = sina*delx+cosa*dely+cy
+		return (resultx,resulty)
+
+	angle = 2*math.pi*circlepercent
+	flipper = -1 if intangent*purex > 0 else 1 #multiply by purex because we want to re-flip here if we flipped due to "turnleft"
+	therange = range(0,100) if intangent > 0 else range(-99,1)
+	points = [intermediatePoint(startpoint,(centerx,centery),flipper*t*angle*0.01) for t in range(0,100)]
+
+	#Length of circle is, fortunately, easy!  It's simply radius*angle
+	length = angle*radius
+
+	#Returns a list of points and the new edge of the racetrack and the change in length
+	return (points,points[-1],length)
+
+
+def generateStraight(startpoint,length,angle):
+	(startx,starty) = startpoint
+	
+	#Now create a parameterized function in terms of the angle
+	#This is easy - tan(angle) = slope, so y = mt + starty, x = t + startx
+	#The length of this line is delx^2+dely^2 = length^2, so (mt)^2+t^2 = length^2
+	#implying t^2 = length^2/(1+m^2)
+	#So t ranges from 0 to length/sqrt(1+m^2)
+	slope = math.tan(angle)
+	tmax = length/math.sqrt(1+slope*slope)
+
+	if angle*slope < 0:
+		#I don't actually know the geometrical reason why this is needed :/
+		#But if you don't do this, sometimes the line points the wrong way!
+		tmax *=-1
+	
+	#Since we draw the track by placing lines, we only need the endpoints of this!
+	#(For other curves we approximate by a bunch of small lines, so we'd need full data)
+	#points = [(t+startx,slope*t+starty) for t in range(0,int(tmax))]
+	points = [startpoint,(tmax+startx,slope*tmax+starty)]
+
+	#Returns a list of points and the new edge of the racetrack and the change in length
+	return (points,points[-1],length)
+
+def convertPointsToAllPositive(xys):
+	#If we went negative anywhere, shift everything over
+	#Returns shifted points tupled with the range over which the points span
+	#We also want everything converted to an integer!
+	maxnegx = 0
+	maxnegy = 0
+	maxx    = 0
+        maxy    = 0
+
+	for point in xys:
+		(x,y) = point
+		maxnegx = min(x,maxnegx)
+		maxnegy = min(y,maxnegy)
+		maxx    = max(x,maxx)
+		maxy    = max(y,maxy)
+
+	newxys = []
+	padding = 5
+	for point in xys:
+		(x,y) = point
+		newxys.append((int(x-maxnegx)+padding,int(y-maxnegy)+padding))
+
+	return (newxys,int(maxx-maxnegx)+2*padding,int(maxy-maxnegy)+2*padding)
 
 # Bezier code taken from https://stackoverflow.com/questions/246525/how-can-i-draw-a-bezier-curve-using-pythons-pil
 
