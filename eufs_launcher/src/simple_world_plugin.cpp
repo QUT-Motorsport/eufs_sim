05817bdb2bb3d7530df415c982d1f44d2655ae21
Angus Stewart <4700831-Siliconlad@users.noreply.gitlab.com>
2019-11-09 16:42:40 +0000

    Fix code style errors flagged up by roslint in simple_world_plugin.cpp

--- eufs_launcher/src/simple_world_plugin.cpp
+++ eufs_launcher/src/simple_world_plugin.cpp
@@ -1,25 +1,28 @@
+// Copyright (c) 2019 Edinburgh University Formula Student (EUFS)
 #include <gazebo/common/Plugin.hh>
 #include <ros/ros.h>
 
-namespace gazebo {
+namespace gazebo
+{
 
-	class WorldPluginTutorial: public WorldPlugin {
+class WorldPluginTutorial: public WorldPlugin
+{
+  public:
+    WorldPluginTutorial(): WorldPlugin() {}
 
-		public:
-			WorldPluginTutorial(): WorldPlugin() {}
+    void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+    {
+      // Make sure ROS node for Gazebo is init.
 
-			void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf) {
-				//Make sure ROS node for Gazebo is init.
+      if (!ros::isInitialized())
+      {
+        ROS_FATAL_STREAM("Ros ain't initialized, so it can't load simple_world_plugin.  " +
+                         "Load the Gazebo system plugin 'libgazebo_ros_api_plugin.so' in the gazebo_ros package.");
+        return;
+      }
 
-				if (!ros::isInitialized()) {
-					ROS_FATAL_STREAM("Ros ain't initialized, so it can't load simple_world_plugin.  Load the Gazebo system plugin 'libgazebo_ros_api_plugin.so' in the gazebo_ros package.");
-					return;
-				}
-
-				ROS_INFO("Hello World!  This is deprecated, I think.");
-
-			}
-
-	};
-	GZ_REGISTER_WORLD_PLUGIN(WorldPluginTutorial);
-}
+      ROS_INFO("Hello World!  This is deprecated, I think.");
+    }
+};
+GZ_REGISTER_WORLD_PLUGIN(WorldPluginTutorial);
+}  // namespace gazebo
