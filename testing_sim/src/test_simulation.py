5ebe5a5dbe44d3658d84505a09abf6bc97abd075
Niklas Burggraaff s1902977 <s1902977@ed.ac.uk>
2019-11-26 22:19:29 +0000

    Made the test node publish the autocross state until the cars state changes from OFF (to READY)

--- testing_sim/src/test_simulation.py
+++ testing_sim/src/test_simulation.py
@@ -28,11 +28,17 @@ if __name__ == '__main__':
 
     rospy.init_node('test_lapcount')
 
+    state = 0
+
     pub = rospy.Publisher('/ros_can/set_mission', CanState, queue_size=1)
     mission_msg = CanState()
     mission_msg.as_state = 0
     mission_msg.ami_state = 13
     mission_msg.mission_flag = False
-    pub.publish(mission_msg)
+
+    while state == 0:
+        msg = rospy.wait_for_message('/ros_can/state', CanState)
+        state = msg.as_state
+        pub.publish(mission_msg)
 
     rostest.rosrun('testing_sim', 'test_simulation', SimulationTestClass)
