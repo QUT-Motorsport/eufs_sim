6ac1fa603634d6311f3a24b3094a5b703e7ac11b
Niklas Burggraaff s1902977 <s1902977@ed.ac.uk>
2020-02-07 17:55:41 +0000

    Added a test for acceleration, inspection, skidpad and track_drive. At the moment the tests for acceleration, inspection and skidpad do not look at anything apart from the as_state going to finished The acceleration, inspection and skidpad missions do not pass the test

--- testing_sim/src/test_inspection.py
+++ testing_sim/src/test_inspection.py
@@ -10,23 +10,23 @@ from eufs_msgs.msg import CanState
 
 class SimulationTestClass(unittest.TestCase):
 
-    lap_count = -1
+    # lap_count = -1
     as_state = 0
 
-    def laps_callback(self, msg):
-        self.lap_count = msg.data
+    # def laps_callback(self, msg):
+    #     self.lap_count = msg.data
 
     def state_callback(self, msg):
         self.as_state = msg.as_state
 
     def test_lapcount(self):
-        rospy.Subscriber('/finish_line_detector/completed_laps', Int16, self.laps_callback)
+        # rospy.Subscriber('/finish_line_detector/completed_laps', Int16, self.laps_callback)
         rospy.Subscriber('/ros_can/state', CanState, self.state_callback)
 
         # Wait until the mission is completed or ros is shutdown
         # There is a timeout in the launch file
-        while not rospy.is_shutdown() and not (self.lap_count == 1 and self.as_state == 4):
-            continue
+        while not rospy.is_shutdown() and not self.as_state == 4: # (self.lap_count == 10 and self.as_state == 4):
+            rospy.sleep(0.1)
 
         self.assertEqual(self.lap_count, 1)
         self.assertEqual(self.as_state, 4)
@@ -34,7 +34,7 @@ class SimulationTestClass(unittest.TestCase):
 
 if __name__ == '__main__':
 
-    rospy.init_node('test_lapcount')
+    rospy.init_node('test_autocross')
 
     pub = rospy.Publisher('/ros_can/set_mission', CanState, queue_size=1)
 
@@ -44,7 +44,7 @@ if __name__ == '__main__':
     # Publish a message to the car to signify the mission
     mission_msg = CanState()
     mission_msg.as_state = 0
-    mission_msg.ami_state = 13 # Autocross
+    mission_msg.ami_state = 16 # Autocross
     mission_msg.mission_flag = False
     pub.publish(mission_msg)
 
