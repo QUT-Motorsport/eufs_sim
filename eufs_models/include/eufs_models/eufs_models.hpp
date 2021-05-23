8e7e8dbe492a02582cb8d191ae965d9aa3bf88a8
Siliconlad <siliconlad@protonmail.com>
2021-05-23 19:23:23 +0100

    Add meta header file for eufs_models
    This header file will allow users of the library to simply include one
    header file to gain access to everything that the library offers.

---
+++ eufs_models/include/eufs_models/eufs_models.hpp
@@ -0,0 +1,11 @@
+#ifndef EUFS_MODELS_HPP
+#define EUFS_MODELS_HPP
+
+#include "eufs_models/vehicle_input.hpp"
+#include "eufs_models/vehicle_param.hpp"
+#include "eufs_models/vehicle_state.hpp"
+#include "eufs_models/vehicle_model.hpp"
+#include "eufs_models/dynamic_bicycle.hpp"
+#include "eufs_models/point_mass.hpp"
+
+#endif // EUFS_MODELS_HPP
