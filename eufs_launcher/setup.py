26e366dd6bf563b574f810cdfa09c55c602393fc
Cameron Matthew <s2020336@ed.ac.uk>
2021-05-27 17:29:28 +0000

    Split the track generator from the launcher into separate packages

--- eufs_launcher/setup.py
+++ eufs_launcher/setup.py
@@ -8,28 +8,20 @@ setup(
     packages=[package_name],
     package_dir={'': 'src'},
     data_files=[
-        ('share/' + package_name, ['launch/eufs_launcher.launch', 'launch/simulation.launch']),
+        ('share/' + package_name,
+         ['launch/eufs_launcher.launch.py', 'launch/simulation.launch.py']),
         ('share/' + package_name + '/config', ['config/eufs_launcher.yaml']),
-        ('share/' + package_name + '/resource', [
-            'resource/Launcher.ui',
-            'resource/randgen_launch_template',
-            'resource/randgen_world_template',
-            'resource/noiseFiles.txt'
-        ]),
-        ('share/' + package_name + '/resource/randgen_model_template', [
-            'resource/randgen_model_template/model.config',
-            'resource/randgen_model_template/model.sdf'
-        ]),
+        ('share/' + package_name + '/resource', ['resource/launcher.ui']),
         ('share/' + package_name, ['plugin.xml']),
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+         ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools', 'scipy', 'pandas'],
     zip_safe=True,
     maintainer='Angus Stewart',
     maintainer_email='siliconlad@protonmail.com',
-    description='The package that launches and links everything together',
+    description='This package Launches the simulation.',
     license='MIT',
     tests_require=['pytest'],
     scripts=['scripts/eufs_launcher'],
