b7fd807180b96f9639fd25cd0f9f5d242b46680b
Luke <luke.krenske3@gmail.com>
2023-02-13 16:03:30 +1000

    2022_CompetitionTrack Addition

--- eufs_tracks/eufs_tracks/track_creator/track_creator.py
+++ eufs_tracks/eufs_tracks/track_creator/track_creator.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2 as cv
 import sys
 import matplotlib.pyplot as plt
+import pandas as pd
 
 def linear_interpolation(track, n_points):
     #attempts to place evently spaced points along the coordinates given
@@ -35,23 +36,26 @@ def rotate_track(track, angle):
     return rotated_track
 
 def remove_resolution(track, resolution):
-    return track[::resolution]
+    return track[::resolution] #removes every n'th point 
 
 def scale_track(track):
-    scaled_track = track*scale_factor
+    scaled_track = track*scale_factor 
     return scaled_track
 
 def track_processing():
-    img_orig = cv.imread("SaudiArabian_GP.jpg")#import image 
+    img_orig = cv.imread("Bahrain_GP.png")#import image 
     threshold_level,img = cv.threshold(img_orig,155,255,0) #clean track to strictly black and white 
     img = cv.cvtColor(img, cv.COLOR_BGR2GRAY) #converts image into grayscape, cv.BGR2GRAY is a preset parameter from opencv  
     contours, hierarchy = cv.findContours(img, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE) #find contors of two sides of track cv.RETR_TREE and cv.CHAIN_APPROX_SIMPLE are preset parameters from opencv
     return contours
 
 #Set parameters 
-n_points = 15000
-scale_factor = 0.05
-angle_rot = 180
+n_points = 12000   #if n_points is not large enough the cone count will not be reached 
+desired_cones = 80
+point_devider = int(n_points / desired_cones)
+scale_factor = 0.095  #for the track converter 1 pixle is one meter therefore small scale factors will be necessary  
+angle_rot = 180 #useful for aligning the starting line on a vertical axis 
+
 
 contours = track_processing()
 
@@ -59,7 +63,7 @@ contours = track_processing()
 blue = contours[1] #this should be the outside of the track line (if not change this number)
 blue = linear_interpolation(blue, n_points)
 blue = scale_track(blue)
-blue = remove_resolution(blue, 55)
+blue = remove_resolution(blue, point_devider)
 blue = rotate_track(blue, angle_rot)
 
 blue_x = blue[:,0]
@@ -69,11 +73,17 @@ blue_y = blue[:,1]
 yellow = contours[2] #this should be the inside of the track line (if not change this number)
 yellow = linear_interpolation(yellow, n_points)
 yellow = scale_track(yellow)
-yellow = remove_resolution(yellow, 55)
+yellow = remove_resolution(yellow, point_devider)
 yellow = rotate_track(yellow, angle_rot)
 
 yellow_x = yellow[:,0]
 yellow_y = yellow[:,1]
+
+track_created = blue,yellow
+
+track_created = pd.DataFrame({i: {j: track_created[i][j] for j in range(len(track_created[i]))} for i in range(len(track_created))})
+track_created.to_csv('3d_array.csv', index=False)
+
 
 #plotting
 plt.scatter(yellow_x,yellow_y)
