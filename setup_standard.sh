a8cdbf80b1c336e3994c3dbd16008c3f916a668a
Liam Johns <liam@mailserv.com.au>
2026-07-08 12:44:38 +1000

    Minor fixes to setup_standard.sh

--- setup_standard.sh
+++ setup_standard.sh
@@ -28,7 +28,7 @@ echo ""
 echo "---Cloning Driverless repo---"
 echo ""
 sleep 3
-git clone --recurse-submodules https://github.com/QUT-Motorsport/QUTMS_Driverless.git
+git clone -b feature/eufs_sim_vehicle_urdf --recurse-submodules https://github.com/QUT-Motorsport/QUTMS_Driverless.git
 cd QUTMS_Driverless
 
 ## Set ROS2 repositories
@@ -78,6 +78,7 @@ echo "Driverless repo installed."
 ## Eufs sim setup. Installation from the migration branch.
 git clone https://github.com/QUT-Motorsport/eufs_sim -b feature/jazzy_migration
 
+## Exports for sim compatibility
 export GZ_SIM_RESOURCE_PATH=$GZ_SIM_RESOURCE_PATH:$QUTMS_WS/install/eufs_tracks/share/eufs_tracks/
 export GZ_SIM_RESOURCE_PATH=$GZ_SIM_RESOURCE_PATH:$QUTMS_WS/eufs_sim/eufs_tracks/meshes
 export GZ_SIM_RESOURCE_PATH=$GZ_SIM_RESOURCE_PATH:$QUTMS_WS/eufs_sim/eufs_tracks/models
@@ -85,13 +86,8 @@ export GZ_SIM_RESOURCE_PATH=$GZ_SIM_RESOURCE_PATH:$QUTMS_WS/eufs_sim/eufs_tracks
 export GZ_SIM_RESOURCE_PATH=$GZ_SIM_RESOURCE_PATH:$QUTMS_WS/eufs_sim/eufs_models/models
 export GZ_SIM_RESOURCE_PATH=$GZ_SIM_RESOURCE_PATH:$QUTMS_WS/install/eufs_tracks/share/eufs_tracks/models
 export GZ_SIM_RESOURCE_PATH=$GZ_SIM_RESOURCE_PATH:$QUTMS_WS/install/eufs_models/share/eufs_models/models
-export GZ_SIM_RESOURCE_PATH=$GZ_SIM_RESOURCE_PATH:$QUTMS_WS/eufs_sim/eufs_tracks/models
-export GZ_SIM_RESOURCE_PATH=$GZ_SIM_RESOURCE_PATH:$QUTMS_WS/eufs_sim/eufs_tracks/worlds
-export GZ_SIM_RESOURCE_PATH=$GZ_SIM_RESOURCE_PATH:$QUTMS_WS/eufs_sim/eufs_models/models
 export GZ_SIM_RESOURCE_PATH=$GZ_SIM_RESOURCE_PATH:$QUTMS_WS/eufs_sim/eufs_models/meshes
-export GZ_SIM_RESOURCE_PATH=$GZ_SIM_RESOURCE_PATH:$QUTMS_WS/install/eufs_tracks/share/eufs_tracks/models
 export GZ_SIM_RESOURCE_PATH=$GZ_SIM_RESOURCE_PATH:$QUTMS_WS/install/eufs_tracks/share/eufs_tracks/worlds
-export GZ_SIM_RESOURCE_PATH=$GZ_SIM_RESOURCE_PATH:$QUTMS_WS/install/eufs_models/share/eufs_models/models
 export GZ_SIM_RESOURCE_PATH=$GZ_SIM_RESOURCE_PATH:$QUTMS_WS/install/eufs_models/share/eufs_models/meshes
 export LIBGL_ALWAYS_SOFTWARE=1
 export GZ_SIM_RESOURCE_PATH=$QUTMS_WS/eufs_sim/eufs_tracks:$QUTMS_WS/eufs_sim/eufs_tracks/worlds:$QUTMS_WS/eufs_sim/eufs_tracks/models:$QUTMS_WS/eufs_sim/eufs_tracks/meshes:$QUTMS_WS/eufs_sim/eufs_msg/msg/proto/gz/driverless_msg:$QUTMS_WS/build/eufs_msg/msg 
@@ -105,7 +101,7 @@ sudo apt install -y python3-pip pre-commit cmake software-properties-common curl
 sudo add-apt-repository universe
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
-sudo apt update
+sudo apt update 
 sudo apt install -y ros-jazzy-desktop ros-dev-tools
 sudo apt install ros-jazzy-ros-gz
 
@@ -115,6 +111,6 @@ source /opt/ros/jazzy/setup.bash
 
 echo ""
 echo "---Setup Completed---"
-ech0 "Please restart WSL"
+echo "Please restart WSL"
 echo ""
 sleep 3
